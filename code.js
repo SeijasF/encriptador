b2e93484d9fc18401e0b31a88ce807f3391168a2
Seijas <virginia78se@gmail.com>
2024-08-11 02:03:03 -0300

    se actualizo estilos, js y html

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
+let mensajeUsuario = document.querySelector('.mensajeUsuario');
 let mensajeEncriptado = document.querySelector('.mensajeEncriptado');
-copiarBtn = document.querySelector('.btnCopiar');
+let copiarBtn = document.querySelector('.btnCopiar');
+let imagen = document.querySelector('.imagen');
+let imagenTexto = document.querySelector('.imagenTexto');
 
 /*La letra "e" es convertida para "enter"
  La letra "i" es convertida para "imes"
@@ -8,11 +11,13 @@ copiarBtn = document.querySelector('.btnCopiar');
  La letra "u" es convertida para "ufat"*/
 
  function encriptar(){
-    let mensajeUsuario = document.querySelector('.mensajeUsuario');
     let mensaje = encriptarMensaje(mensajeUsuario.value);
     mensajeEncriptado.value = mensaje;
     mensajeUsuario.value = "";
     copiarBtn.classList.remove('copiar');
+    imagen.classList.add('img');
+    imagenTexto.classList.add('imagen_texto')
+
  }
 
  function encriptarMensaje(msg) {
@@ -24,3 +29,34 @@ copiarBtn = document.querySelector('.btnCopiar');
     }
     return msg;
  } 
+
+ function copiarMensaje() {
+    let msg = mensajeEncriptado.value;
+    navigator.clipboard.writeText(msg);
+ }
+
+ function desencriptar() {
+    let mensajeUsuario = document.querySelector('.mensajeUsuario');
+    let mensaje = desencriptarMensaje(mensajeUsuario.value);
+    mensajeEncriptado.value = mensaje;
+    mensajeUsuario.value = "";
+    copiarBtn.classList.remove('copiar');
+ }
+
+ function desencriptarMensaje(msg) {
+    const matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    for(i=0; i < matrizCodigo.length; i++){
+        if(msg.includes(matrizCodigo[i][1])){
+            msg = msg.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return msg;
+ }
+
+ function reiniciarEncriptador(){
+    mensajeUsuario.value = "";
+    mensajeEncriptado.value = "";
+    copiarBtn.classList.add('copiar');
+    imagen.classList.remove('img');
+    imagenTexto.classList.remove('imagen_texto')
+}
